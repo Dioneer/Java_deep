a1679054df55baf14bc70efce7cf583a46d2b9cc
Dioneer <gulkina_em@mail.ru>
2023-11-04 00:12:24 +0300

    array stream

--- src/serialize/ReadObj.java
+++ src/serialize/ReadObj.java
@@ -4,14 +4,29 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.Arrays;
 
 public class ReadObj {
     public static void main(String[] args) {
+//        try(FileInputStream fis = new FileInputStream("people.bin"); ObjectInputStream ois = new ObjectInputStream(fis )){
+//            Person person1 = (Person) ois.readObject();
+//            Person person2 = (Person) ois.readObject();
+//            System.out.println(person1);
+//            System.out.println(person2);
+//        } catch (FileNotFoundException e) {
+//            System.out.println(e.getMessage());
+//        }catch(ClassNotFoundException e){
+//            System.out.println(e.getMessage());
+//        }catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
         try(FileInputStream fis = new FileInputStream("people.bin"); ObjectInputStream ois = new ObjectInputStream(fis )){
-            Person person1 = (Person) ois.readObject();
-            Person person2 = (Person) ois.readObject();
-            System.out.println(person1);
-            System.out.println(person2);
+            int i = ois.readInt();
+            Person[] person = new Person[i];
+            for (int j = 0; j < i; j++) {
+                person[j] = (Person) ois.readObject();
+            }
+            System.out.println(Arrays.toString(person));
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }catch(ClassNotFoundException e){
--- src/serialize/WriteObj.java
+++ src/serialize/WriteObj.java
@@ -7,12 +7,23 @@ import java.io.ObjectOutputStream;
 
 public class WriteObj {
     public static void main(String[] args){
-        Person person1 = new Person(1, "Ira");
-        Person person2 = new Person(2, "Mirra");
-
-       try(FileOutputStream fos = new FileOutputStream("people.bin"); ObjectOutputStream oos = new ObjectOutputStream(fos)){
-            oos.writeObject(person1);
-           oos.writeObject(person2);
+//        Person person1 = new Person(1, "Ira");
+//        Person person2 = new Person(2, "Mirra");
+//
+//       try(FileOutputStream fos = new FileOutputStream("people.bin"); ObjectOutputStream oos = new ObjectOutputStream(fos)){
+//            oos.writeObject(person1);
+//           oos.writeObject(person2);
+//       }catch(FileNotFoundException e){
+//           System.out.println(e.getMessage());
+//       }catch(IOException e){
+//           System.out.println(e.getMessage());
+//       }
+          Person[] people= {new Person(1, "Ira"), new Person(2, "Mirra")};
+          try(FileOutputStream fos = new FileOutputStream("people.bin"); ObjectOutputStream oos = new ObjectOutputStream(fos)){
+            oos.writeInt(people.length);
+              for (Person person: people) {
+                  oos.writeObject(person);
+              }
        }catch(FileNotFoundException e){
            System.out.println(e.getMessage());
        }catch(IOException e){
